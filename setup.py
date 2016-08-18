6931f4bdacf343759f4db564940b21c4219cc955
leoGalani <leo@keeptesting.com.br>
2016-08-19 00:29:50 +0200

    added coded for status

--- setup.py
+++ setup.py
@@ -4,14 +4,30 @@ from sherlock.data.model import User, State
 
 db.create_all()
 
-initial_user = User(name="Sherlock", username="sherlock", password="123")
-
-# states
-available_state = State(name="available")
-disable_state = State(name="disable")
-
-# do not change the order of the itens creation
-db.session.add(initial_user)
+# DONT CHANGE THE CODE BELOW.
+initial_user = User(name="Leonardo", username="leo", password="let")
+available_state = State(name="active", code="ACTIVE")
 db.session.add(available_state)
+db.session.add(initial_user)
+db.session.commit()
+# DONT CHANGE THE CODE ABOVE.
+
+# general states
+disable_state = State(name="disable", code="DISABLE")
+
+# test_case_states
+not_executed_state = State(name="not executed", code="NOT_EXECUTED")
+passed_state = State(name="passed", code="PASSED")
+error_state = State(name="error", code="ERROR")
+blocked_state = State(name="blocked", code="BLOCKED")
+
+# cycle states
+closed_cycle = State(name="closed", code="CLOSED")
+
 db.session.add(disable_state)
+db.session.add(not_executed_state)
+db.session.add(passed_state)
+db.session.add(error_state)
+db.session.add(blocked_state)
+db.session.add(closed_cycle)
 db.session.commit()
