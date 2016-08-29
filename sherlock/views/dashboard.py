ebc778ffadce10ec0206651b07f18b1a70f3e9ed
leoGalani <leo@leonardobg.com.br>
2016-08-29 21:02:31 +0200

    add display of new info on the sherlock dashboard

--- sherlock/views/dashboard.py
+++ sherlock/views/dashboard.py
@@ -2,10 +2,15 @@
 from flask import Blueprint, request, url_for, redirect, g, render_template
 from flask_login import login_required
 
+from sherlock.data.model import Cycle, CycleHistory
+from sherlock.helpers.util import load_last_cyle_status_of_projects
+
 dashboard = Blueprint('dashboard', __name__)
 
 @dashboard.route('/', methods=['GET'])
 @login_required
 def home():
-    """Return a user."""
+    if g.projects:
+        g.last_project_cycles = load_last_cyle_status_of_projects(
+            Cycle, CycleHistory, g.projects)
     return render_template("dashboard.html")
