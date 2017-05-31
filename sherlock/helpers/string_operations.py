d58ccce06a53444709035743041c7d56d3928160
LeoGalani <leo@leonardobg.com.br>
2017-05-31 16:39:44 +0200

    check non and blank for list too

--- sherlock/helpers/string_operations.py
+++ sherlock/helpers/string_operations.py
@@ -17,9 +17,14 @@ def is_empty(obj):
     return len(obj) == 0
 
 
-def check_str_none_and_blank(string, name):
+def check_none_and_blank(obj, name):
     name = name.upper()
-    if string is None:
+    if obj is None:
         abort(make_response(jsonify(message='MISSING_{}'.format(name)), 400))
-    if string.strip() == '':
-        abort(make_response(jsonify(message='BLANK_{}'.format(name)), 400))
+
+    if type(obj) is str:
+        if obj.strip() == '':
+            abort(make_response(jsonify(message='BLANK_{}'.format(name)), 400))
+    elif type(obj) is list:
+        if len(obj) == 0:
+            abort(make_response(jsonify(message='EMPTY_{}_LIST'.format(name)), 400))
