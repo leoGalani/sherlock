5e7263208a8e20efda94a61e8a78bac58a1542b5
leoGalani <leo@leonardobg.com.br>
2017-08-17 17:09:03 +0200

    added cors only for dev

--- runserver.py
+++ runserver.py
@@ -1,6 +1,8 @@
 """Server runner instance."""
+from flask_cors import CORS
 
 from sherlockapi import app
 
 if __name__ == '__main__':
+    CORS(app, resources={r'/*': {"origins": '*', 'allow_headers': '*'}})
     app.run(host="0.0.0.0", port=8080)
