b0f6504dcb563385302ba3fe7110ee2db9552154
leoGalani <leo@leonardobg.com.br>
2017-08-18 19:46:09 +0200

    issue #19

--- sherlockapi/helpers/util.py
+++ sherlockapi/helpers/util.py
@@ -20,8 +20,13 @@ def count_cycle_stats(casesCycle):
     current_cycles_stats['total_blocked'] = 0
     current_cycles_stats['total_passed'] = 0
 
+    current_cycles_stats['total'] = 0
+
     for item in casesCycle:
         current_cycles_stats['total_{}'.format(item.state_code.value)] += 1
+        current_cycles_stats['total'] += 1
+
+
 
     return current_cycles_stats
 
