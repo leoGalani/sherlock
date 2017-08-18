d84fb79de47b4937ca406c5e32fb32979f1d76c8
leoGalani <leo@leonardobg.com.br>
2017-08-18 17:34:40 +0200

    useless file

--- frontsherlock/src/config.js
+++
@@ -1,8 +0,0 @@
-export const getHeader = function () {
-  const tokenData = JSON.parse(window.localStorage.getItem('authUser'))
-  const headers = {
-    Accept: 'application/json',
-    Authorization: 'Basic ' + btoa(tokenData.token + ':')
-  }
-  return headers
-}
