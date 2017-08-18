0a1b99494b3eb616307a3e0defa3e9a660aba3ba
leoGalani <leo@leonardobg.com.br>
2017-08-18 17:46:57 +0200

    changed version file

--- frontsherlock/src/Footer.vue
+++ frontsherlock/src/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="menu_Component" style="padding-top:20px">
-    <center><span style="font-size:12px"> sherlock qa testmanager - v1.0.0 | <a uk-icon="icon: github-alt" href="https://github.com/leoGalani/sherlock"> source code </a></span></center>
+    <center><span style="font-size:12px"> sherlock qa testmanager - v1.0.1 | <a uk-icon="icon: github-alt" href="https://github.com/leoGalani/sherlock"> source code </a></span></center>
   </div>
 </template>
 
