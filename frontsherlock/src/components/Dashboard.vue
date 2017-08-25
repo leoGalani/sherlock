021e150ced219de19acad2b13a5ddef8c62706da
leoGalani <leo@leonardobg.com.br>
2017-08-25 22:33:17 +0200

    #14

--- frontsherlock/src/components/Dashboard.vue
+++ frontsherlock/src/components/Dashboard.vue
@@ -4,12 +4,34 @@
        <center><div uk-spinner></div>
        Loading...</center>
      </div>
-    <h2 v-if="showTitle"> Current Projects </h2>
 
-    <div v-for="project in projects.projects" class="project_box">
+     <div class="uk-grid" v-if="showTitle">
+       <div class="uk-width-4-5" style="width:65% !important">
+         <h2 style="margin-left: 18px;"> Current Projects </h2>
+       </div>
+       <div class="uk-width-1-5" style="width:35% !important; text-align: right; padding-right: 14px;">
+         <span><i class="material-icons">filter_list</i></span>
+         <span>
+         <a class="uk-button  uk-button-default filter_state"
+            v-bind:class="{ filter_state_active: filterActive }"
+            @click="filterActive = !filterActive, filterClick('active')"> active </a>
+         <a class="uk-button uk-button-default filter_state"
+            v-bind:class="{ filter_state_active: filterClose }"
+            @click="filterClose = !filterClose, filterClick('closed')""> closed </a>
+         <a class="uk-button uk-button-default filter_state"
+            v-bind:class="{ filter_state_active: filterNoCycle }"
+            @click="filterNoCycle = !filterNoCycle, filterClick('nocycle')""> no cycle </a>
+       </span>
+       </div>
+     </div>
+
+
+    <transition-group>
+    <div v-for="project in projects.projects" :key="project.id" class="project_box" v-if="filter.length === 0 || filter.indexOf(project.cycle_state) > -1">
       <router-link :to="{ path: 'project/view/'+project.id }" class="box-link">
         <h4 class="hide_overflow">{{project.name}}</h4>
         <hr>
+
         <span v-if="project.have_cycle === true">
           <span> Current Cycle:  {{project.current_cycle}} </span> <br>
           <span> Cycle State: {{project.cycle_state}} </span> <br>
@@ -24,6 +46,7 @@
         </span>
       </router-link>
     </div>
+  </transition-group>
 
     <div v-if="showGreetings">
       <hr>
@@ -44,7 +67,11 @@ export default {
       projects: [],
       loading: false,
       showTitle: false,
-      showGreetings: false
+      showGreetings: false,
+      filterActive: false,
+      filterClose: false,
+      filterNoCycle: false,
+      filter: []
     }
   },
   methods: {
@@ -60,6 +87,14 @@ export default {
           this.showTitle = false
         }
       })
+    },
+    filterClick: function (item) {
+      if (this.filter.indexOf(item) > -1) {
+        var index = this.filter.indexOf(item)
+        this.filter.splice(index, 1)
+      } else {
+        this.filter.push(item)
+      }
     }
   },
   created: function () {
@@ -72,6 +107,7 @@ export default {
   updated: function () {
     //  this.fetchProjects()
   },
+
   beforeDestroy: function () {
     clearInterval(this.interval)
   }
@@ -80,12 +116,75 @@ export default {
 
 <style scoped>
 
-.dashboard div{
-  padding: 10px;
-}
-
 .project_box{
   min-height: 210px;
+}
+
+.filter_state {
+  border: solid 1px #333;
+  border-radius: 7px;
+  padding: 0px 20px;
+  line-height: 28px;
+  text-transform: none;
+}
+
+.filter_state:hover{
+    font-weight: 500;
+    z-index: -1;
+    transform: scale(1);
+    box-shadow: 3px 4px 7px -4px #000000;
+    -webkit-box-shadow: 3px 4px 7px -4px #000000;
+    -moz-box-shadow: 3px 4px 7px -4px #000000;
+    box-shadow: 3px 4px 7px -4px #444444;
+}
+
+.filter_state_active{
+    color: #fff !important;
+    background-color: #2f5c86;
+    font-weight: 500;
+    z-index: -1;
+    transform: scale(.9);
+    box-shadow: 3px 4px 7px -4px #000000;
+    -webkit-box-shadow: 3px 4px 7px -4px #000000;
+    -moz-box-shadow: 3px 4px 7px -4px #000000;
+    box-shadow: 3px 4px 7px -4px #444444;
+}
+
+
+.animated {
+    display: inline-block;
+}
+
+.animated.v-enter {
+    animation: fadein .5s;
+}
+
+.animated.v-leave {
+    animation: fadeout .5s;
+}
+
+@keyframes fadein {
+    0% {
+        transform: scale(0);
+    }
+    50% {
+        transform: scale(1.5);
+    }
+    100% {
+        transform: scale(1);
+    }
+}
+
+@keyframes fadeout {
+    0% {
+        transform: scale(1);
+    }
+    50% {
+        transform: scale(1.5);
+    }
+    100% {
+        transform: scale(0);
+    }
 }
 
 </style>
